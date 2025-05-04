9129779481e79a93404bd59c8a45999fd861020e
mizanozack <67621220+mizanozack@users.noreply.github.com>
2025-05-04 17:07:17 +0800

    Delete bymaiza-cart directory

--- bymaiza-cart/components/ui/card.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Card({ children, className }) {
-  return <div className={`rounded-xl shadow-md ${className}`}>{children}</div>;
-}
-
-export function CardContent({ children }) {
-  return <div className="p-4">{children}</div>;
-}
