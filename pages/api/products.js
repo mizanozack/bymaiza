2073a6de82aae1c0ab4127fe4b493cac318d1ddb
mizanozack <67621220+mizanozack@users.noreply.github.com>
2025-05-04 16:04:29 +0800

    Delete pages directory

--- pages/api/products.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const products = [
-    { id: 1, name: 'Coffee', price: 12.99 },
-    { id: 2, name: 'Headphones', price: 89.99 },
-  ];
-  res.status(200).json(products);
-}
